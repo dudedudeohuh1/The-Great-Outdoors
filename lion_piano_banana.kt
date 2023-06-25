ec50ccf2467bc2ff64074191bfb7a869ff154bdb
dudedudeohuh1 <136755406+dudedudeohuh1@users.noreply.github.com>
2023-06-25 14:46:06 +0700

    Improve error handling

---
+++ lion_piano_banana.kt
@@ -0,0 +1,499 @@
+//1
+fun hikeUpTheMountainTop(){
+    println("Let's explore the great outdoors!")
+}
+
+//2
+fun kayakDownTheRiver(){
+    println("Explore the tranquil passageway!")
+}
+
+//3
+fun takeABicycleTour(){
+    println("Look at the beautiful sights!")
+}
+
+//4
+fun findAPeacefulLake(){
+    println("Take in the serenity of nature!")
+}
+
+//5
+fun bikeAlongTheCoast(){
+    println("Gain perspective from the sea!")
+}
+
+//6
+fun visitTheBeach(){
+    println("Soak up the rays of sunshine!")
+}
+
+//7
+fun takeAForestWalk(){
+    println("Listen to the sounds of nature!")
+}
+
+//8
+fun climbACliffFace(){
+    println("Experience the thrill of the ascent!")
+}
+
+//9
+fun doAGlacierTrek(){
+    println("Admire the beauty of the ice!")
+}
+
+//10 
+fun exploreACave(){
+    println("Discover the wonders of the unknown!")
+} 
+
+//11
+fun backpackingTrip(){
+    println("Experience the backcountry!")
+}
+
+//12
+fun cycleThruCanyons(){
+    println("Be inspired by the grandest of views!")
+}
+
+//13
+fun travelToASummerFestival(){
+    println("Enjoy the revelry of outdoor fun!")
+}
+
+//14
+fun reconnoiterAVolcano(){
+    println("Be safe and enjoy the view!")
+}
+
+//15
+fun goFishing(){
+    println("Witness the beauty of the underwater life!")
+}
+
+//16
+fun catchAndRelease(){
+    println("Protect our beautiful environment!")
+}
+
+//17
+fun raftDownTheRiver(){
+    println("Feel the peacefulness of a lazy river!")
+}
+
+//18
+fun takeAPicnic(){
+    println("Delight in the bounty of nature!")
+}
+
+//19
+fun huntForWildflowers(){
+    println("Find the hidden beauty of nature!")
+}
+
+//20
+fun exploreAJungle(){
+    println("Experience the thrill of the unknown!")
+}
+
+//21
+fun campOutUnderTheStars(){
+    println("Gaze at the beauty of the night sky!")
+}
+
+//22
+fun watchANatureDocumentary(){
+    println("Learn about the wonders around you!")
+} 
+
+//23
+fun pickBerries(){
+    println("Savor the sweet treats nature has to offer!")
+}
+
+//24
+fun takeAPaddleboardTour(){
+    println("Gain a unique perspective of the outdoors!")
+}
+
+//25
+fun takeAPortraitInNature(){
+    println("Capture the beauty of the outdoors!")
+}
+
+//26
+fun goBirdWatching(){
+    println("Spy on nature's feathered friends!")
+}
+
+//27
+fun exploreATrail(){
+    println("Walk along rivers, woods, and paths!")
+}
+
+//28
+fun paddleInACanoe(){
+    println("Navigate the waterways of the world!")
+}
+
+//29
+fun visitAnAquarium(){
+    println("See the beauty of aquatic life!")
+}
+
+//30
+fun checkOutAnObservatory(){
+    println("Look to the heavens for answers!")
+}
+
+//31
+fun playFrisbeeOnTheBeach(){
+    println("Have fun under the sun!")
+}
+
+//32
+fun takeAnEcosystemTour(){
+    println("Learn about the environment around you!")
+}
+
+//33
+fun goScubaDiving(){
+    println("Discover the secrets of the deep!")
+}
+
+//34
+fun setUpATent(){
+    println("Fall asleep under the night sky!")
+}
+
+//35
+fun rentABoat(){
+    println("Explore the depths of the ocean!")
+}
+
+//36
+fun takeALandscapePhoto(){
+    println("Capture the beauty of nature!")
+}
+
+//37
+fun visitAMarineSanctuary(){
+    println("Help protect the beauty of the ocean!")
+}
+
+//38
+fun observeWildlife(){
+    println("Learn from the creatures of nature!")
+}
+
+//39
+fun exploreACoastalTown(){
+    println("Experience the culture of the locals!")
+}
+
+//40
+fun goOnAHorsebackAdventure(){
+    println("Travel by a different means!")
+}
+
+//41
+fun visitAHistoricPark(){
+    println("Learn about days gone by!")
+}
+
+//42
+fun haveAWaterFight(){
+    println("Cool off and have fun!")
+}
+
+//43
+fun goSkiing(){
+    println("Enjoy winter sports to the fullest!")
+}
+
+//44
+fun exploreAMountainRange(){
+    println("Experience the grandeur of the peaks!")
+}
+
+//45
+fun takeATreeClimbingLesson(){
+    println("Explore the heights of the jungle!")
+}
+
+//46
+fun swimInACave(){
+    println("Dive into the depths of the earth!")
+}
+
+//47
+fun goWindsurfing(){
+    println("Challenge yourself to the wind!")
+}
+
+//48
+fun paddleATwoPersonKayak(){
+    println("Explore with a buddy!")
+}
+
+//49
+fun takeAHotAirBalloonRide(){
+    println("Rise up to the clouds!")
+}
+
+//50
+fun tryGeocaching(){
+    println("Go on a treasure hunt!")
+}
+
+//51
+fun takeASereneWalk(){
+    println("Calm your mind with nature!")
+}
+
+//52
+fun goParagliding(){
+    println("Soar on the wings of the wind!")
+}
+
+//53
+fun takeAMountainBikingTrip(){
+    println("Experience the thrill of off-road adventure!")
+}
+
+//54
+fun exploreAPlantSanctuary(){
+    println("Admire the beauty of botanical life!")
+}
+
+//55
+fun stargazing(){
+    println("See the beauty of the night sky!")
+}
+
+//56
+fun hitTheSkiSlopes(){
+    println("Slide down the mountains!")
+}
+
+//57
+fun goKiteFlying(){
+    println("Let the wind carry the fun!")
+}
+
+//58
+fun exploreATunnel(){
+    println("Don't leave the path of light!")
+}
+
+//59
+fun takeASurfingLesson(){
+    println("Ride the waves of the ocean!")
+}
+
+//60
+fun takeASafari(){
+    println("Get up close to nature!")
+}
+
+//61
+fun goKayakFishing(){
+    println("Catch the biggest of fish!")
+}
+
+//62
+fun takeASnowboardingLesson(){
+    println("Make your way down icy slopes!")
+}
+
+//63
+fun takeAPicnicAtTheBeach(){
+    println("Eat lunch with a view!")
+}
+
+//64
+fun takeASunsetStroll(){
+    println("Enjoy the beauty of the day's end!")
+}
+
+//65
+fun takeAPhotographyCourse(){
+    println("Capture the beauty of the outdoors!")
+}
+
+//66
+fun goZipLining(){
+    println("Experience the rush of the wind!")
+}
+
+//67
+fun rideASegway(){
+    println("Take a tour with a twist!")
+}
+
+//68
+fun goRockClimbing(){
+    println("Test your physical and mental limits!")
+}
+
+//69
+fun exploreACornMaze(){
+    println("Find your way to the center!")
+}
+
+//70
+fun goCanoeing(){
+    println("Row your way to adventure!")
+}
+
+//71
+fun hitTheTrails(){
+    println("Rollerblade or cycle your way through!")
+}
+
+//72
+fun goIceClimbing(){
+    println("Be prepared to brave the cold!")
+}
+
+//73
+fun findAPark(){
+    println("Take in the beauty of nature!")
+}
+
+//74
+fun takeAWildlifePhotographyCourse(){
+    println("Capture the beauty of nature!")
+}
+
+//75
+fun takeAWhiteWaterRaftingTour(){
+    println("Experience the thrill of the rapids!")
+}
+
+//76
+fun takeASandboardingLesson(){
+    println("Slide down the sandy dunes!")
+}
+
+//77
+fun goOnAMoonlitNightWalk(){
+    println("Explore the land in the cover of darkness!")
+}
+
+//78
+fun tryGeology(){
+    println("Learn about the formation of the land!")
+}
+
+//79
+fun takeASailboatOut(){
+    println("Gain perspective from the sea!")
+}
+
+//80
+fun getHennaTattoos(){
+    println("Express yourself with body art!")
+}
+
+//81
+fun trySnorkeling(){
+    println("Explore the depths of the ocean!")
+}
+
+//82
+fun goOnAHike(){
+    println("Discover the beauty of nature!")
+}
+
+//83
+fun visitAMarina(){
+    println("Look at the boats and have fun!")
+}
+
+//84
+fun takeACaveTour(){
+    println("Experience the depths of the underground!")
+}
+
+//85
+fun learnArchery(){
+    println("Develop skills and aim for the bulls-eye!")
+}
+
+//86
+fun exploreATrailerPark(){
+    println("Stop by for much-needed rest!")
+}
+
+//87
+fun visitAPlanetarium(){
+    println("See the wonders of the night sky!")
+}
+
+//88
+fun goCrosscountrySkiing(){
+    println("Cruise through the snow!")
+}
+
+//89
+fun joinAMountainClub(){
+    println("Learn from the experts and expand your knowledge!")
+}
+
+//90
+fun takeABroomballLesson(){
+    println("Gather your team and learn the basics!")
+}
+
+//91
+fun mountainBiking(){
+    println("Revel in the beauty of the outdoors!")
+}
+
+//92
+fun goHorseRiding(){
+    println("Feel the beat of the horse's feet!")
+}
+
+//93
+fun takeAWildernessSurvivalCourse(){
+    println("Learn to survive in the wild!")
+}
+
+//94
+fun takeALeonidMeteorShower(){
+    println("See the smoke of comets!")
+}
+
+//95
+fun goSnowshoeing(){
+    println("Walk through the winter wonderland!")
+}
+
+//96
+fun takeAButterflyWatchingTour(){
+    println("See the beauty of nature's fluttering friends!")
+}
+
+//97
+fun exploreACreepyCrawl(){
+    println("Enjoy the spooky fun!")
+}
+
+//98
+fun tryCaving(){
+    println("Discover the wonders of the underground!")
+}
+
+//99
+fun goOffroadDriving(){
+    println("Test your driving skills in a new setting!")
+}
+
+//100
+fun takeAPaintingClass(){
+    println("Capture the beauty of nature on canvas!")
+}
